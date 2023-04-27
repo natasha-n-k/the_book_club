f0f0a60f7dc19e34654d1cd769159ab0d2e5b9ce
Natasha <nnkonovalova@bk.ru>
2023-04-27 18:43:54 +0300

    created admin user

--- the_book_club/club/models.py
+++ the_book_club/club/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class BookClub(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    language = models.CharField(max_length=2)
+
+class ClubPage(models.Model):
+    book_club = models.ForeignKey(BookClub, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    language = models.CharField(max_length=2)
