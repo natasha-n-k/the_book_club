58ebf587550978f0572f3ed73423f3b6071b3fe0
Natasha <nnkonovalova@bk.ru>
2023-05-04 20:11:42 +0300

    corrected static

--- the_book_club/the_book_club/settings.py
+++ the_book_club/the_book_club/settings.py
@@ -127,7 +127,9 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 
