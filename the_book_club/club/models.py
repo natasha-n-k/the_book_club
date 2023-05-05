ff61fefb6c9b5f7933b3b875f8f15df638b4393b
Natasha <nnkonovalova@bk.ru>
2023-05-05 21:11:22 +0300

    added classes BookPage, Book

--- the_book_club/club/models.py
+++ the_book_club/club/models.py
@@ -1,12 +1,29 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
+
+
+fs = FileSystemStorage(location='media')
 
 class BookClub(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     language = models.CharField(max_length=2)
+    image = models.ImageField(upload_to='images', storage=fs)
 
 class ClubPage(models.Model):
     book_club = models.ForeignKey(BookClub, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    language = models.CharField(max_length=2)
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    language = models.CharField(max_length=2)
+    image = models.ImageField(upload_to='images', storage=fs)
+
+class BookPage(models.Model):
+    book = models.ForeignKey(BookClub, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
     language = models.CharField(max_length=2)
