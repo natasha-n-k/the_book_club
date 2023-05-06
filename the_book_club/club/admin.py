e2571718fa7575f788b326244a99ff16ffbdd778
Natasha <nnkonovalova@bk.ru>
2023-05-07 00:59:10 +0300

    added models in admin

--- the_book_club/club/admin.py
+++ the_book_club/club/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import BookClub, ClubPage, Book, BookPage
+
 
 # Register your models here.
+admin.site.register(BookClub)
+admin.site.register(ClubPage)
+admin.site.register(Book)
+admin.site.register(BookPage)
+
