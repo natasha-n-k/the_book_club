934128cdebfe0a061d2183fa241a6e145c3a0e39
Natasha <nnkonovalova@bk.ru>
2023-05-16 15:28:14 +0300

    corrected style register

--- the_book_club/club/forms.py
+++ the_book_club/club/forms.py
@@ -11,3 +11,10 @@ class UserCreationForm(forms.UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
+        labels = {
+            'username': 'Имя пользователя',
+            'password1': 'Пароль',
+            'password2': 'Подтверждение пароля',
+        }
+
+    
