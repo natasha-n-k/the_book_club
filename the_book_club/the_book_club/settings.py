bc6aa6f222538132771ded2164802dc5c54c47ad
Natasha <nnkonovalova@bk.ru>
2023-11-02 15:06:03 +0300

    added django-debug-toolbar

--- the_book_club/the_book_club/settings.py
+++ the_book_club/the_book_club/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +52,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+] 
 
 ROOT_URLCONF = 'the_book_club.urls'
 
--- the_book_club/the_book_club/urls.py
+++ the_book_club/the_book_club/urls.py
@@ -26,3 +26,8 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'club.views.page_not_found'
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),) 
