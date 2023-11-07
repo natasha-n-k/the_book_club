60476275a63afd29e267b8a697c4459ac2f5d368
Natasha <nnkonovalova@bk.ru>
2023-11-07 22:02:24 +0300

    defined API URLs for views

---
+++ the_book_club/api/api/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'bookclubs', views.BookClubViewSet)
+router.register(r'books', views.BookViewSet)
+router.register(r'userbooks', views.UserBookViewSet)
+router.register(r'comments', views.CommentViewSet)
+router.register(r'ratings', views.RatingViewSet)
+router.register(r'queues', views.QueueViewSet)
+router.register(r'meetings', views.MeetingViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
